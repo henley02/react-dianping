42f2afe9d71c23a1b03a3f6da10b37ab6795a468
henley02 <410451447@qq.com>
2018-07-24 13:55:44 +0800

    [+]edit webpack.config

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -55,6 +55,17 @@ module.exports = {
                         }
                     }
                 ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: "font/[name].[ext]"
+                        }
+                    }
+                ]
             }
         ]
     },
